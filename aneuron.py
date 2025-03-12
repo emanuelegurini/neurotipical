a01e46131d686d77e6cb2d03f1812eb851858823
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-03-13 00:55:34 +0100

    add neuron and connection classes

--- aneuron.py
+++ aneuron.py
@@ -2,6 +2,18 @@ j = [1, 2, 3] # input
 w = [-2, -3, -4] # weight
 b = 1
 
+class Neuron():
+	bias = None
+	input = []
+	output = None
+	pass
+
+class Connection():
+	source_neuron = None
+	target_neuron = None
+	weight = None
+	pass
+
 def cni(j, w, b): # cni stands for compute_neuron_input
 	j_with_bias = [1] + j.copy()
 	w_with_bias = [b] + w.copy()
