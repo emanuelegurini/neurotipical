3454ad834968e918bbfc44d73c78bb10bb9078e0
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-03-15 01:03:46 +0100

    add method and properties into connection and neuron classes

--- aneuron.py
+++ aneuron.py
@@ -1,20 +1,44 @@
-j = [1, 2, 3] # input
-w = [-2, -3, -4] # weight
-b = 1
-
 class Neuron():
-	bias = None
-	input = []
-	output = None
-	pass
+	def __init__(self, bias):
+		self.bias = bias
+		self.incoming_connections = []
+
+	def get_bias(self):
+		return self.bias
+	
+	def add_incoming_connection(self, incoming_connection):
+		self.incoming_connections.append(incoming_connection)
+
+	def get_incoming_connections(self):
+			return self.incoming_connections
+	
+	def get_output(self):
+		pass
 
 class Connection():
-	source_neuron = None
-	target_neuron = None
-	weight = None
-	pass
+	def __init__(self, source_neuron, target_neuron, weight):
+		self.source_neuron = source_neuron
+		self.target_neuron = target_neuron
+		self.weight = weight
 
-def cni(j, w, b): # cni stands for compute_neuron_input
+	def get_source_neuron(self):
+		return self.source_neuron
+
+	def get_target_neuron(self):
+		return self.target_neuron
+
+	def get_weight(self):
+		return self.weight
+
+neuron_a = Neuron(bias=1)
+neuron_b = Neuron(bias=1)
+
+ab_conn = Connection(neuron_a, neuron_b, 2)
+neuron_b.add_incoming_connection(ab_conn)
+
+print(neuron_b.get_incoming_connections())
+
+""" def cni(j, w, b): # cni stands for compute_neuron_input
 	j_with_bias = [1] + j.copy()
 	w_with_bias = [b] + w.copy()
 
@@ -31,4 +55,4 @@ def cni(j, w, b): # cni stands for compute_neuron_input
 
 	return max(0, acc)
 
-print(cni(j, w, b))
+print(cni(j, w, b)) """
